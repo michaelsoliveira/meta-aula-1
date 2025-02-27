1f854035f928e64dc24fee1a24589f81bb6d5f1c
Michael Oliveira <michaelsoliveira@gmail.com>
2025-02-26 21:37:04 -0300

    Atualizações

---
+++ src/app/(auth)/welcome.tsx
@@ -0,0 +1,60 @@
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
+
+import { onboarding } from "@/constants";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+const WelcomeScreen = () => {
+    const swiperRef = useRef<Swiper>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const isLastSlide = activeIndex === onboarding.length - 1;
+
+    return (
+        <SafeAreaView className="flex h-full items-center justify-between bg-white">
+            <TouchableOpacity
+                onPress={() => {
+                    router.replace("/(auth)/sign-up");
+                }}
+                className="w-full flex justify-end items-end p-5"
+            >
+                <Text className="text-black text-md">Pular</Text>
+            </TouchableOpacity>
+
+            <Swiper
+                ref={swiperRef}
+                loop={false}
+                dot={
+                    <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+                }
+                activeDot={
+                    <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+                }
+            >
+                {
+                    onboarding.map((item) => (
+                        <View 
+                            key={item.id} 
+                            className="flex items-center justify-center p-5"
+                        >
+                            <Image
+                                source={item.image}
+                                className="w-full h-[300px]"
+                                resizeMode="contain" 
+                            />
+                            <View className="flex flex-row items-center justify-center w-full mt-10">
+                                <Text className="text-black text-3xl font-bold mx-10 text-center">
+                                    {item.title}
+                                </Text>
+                                <Text className="text-[#858585] text-md mx-10 mt-3 text-center">
+                                    {item.description}
+                                </Text>
+                            </View>
+                        </View>
+                    ))
+                }
+            </Swiper>
+        </SafeAreaView>
+    );
+}
---
+++ src/constants/index.ts
@@ -0,0 +1,40 @@
+import onboarding1 from "@assets/images/onboarding1.png";
+import onboarding2 from "@assets/images/onboarding2.png";
+import onboarding3 from "@assets/images/onboarding3.png";
+import getStarted from "@assets/images/get-started.png";
+import signUpCar from "@assets/images/signup-car.png";
+import check from "@assets/images/check.png";
+import noResult from "@assets/images/no-result.png";
+import message from "@assets/images/message.png";
+
+export const images = {
+    onboarding1,
+    onboarding2,
+    onboarding3,
+    getStarted,
+    signUpCar,
+    check,
+    noResult,
+    message
+}
+
+export const onboarding = [
+    {
+        id: 1,
+        title: "A melhor viagem para você",
+        description: "Conforto e segurança em um aplicativo de mobilidade urbana",
+        image: images.onboarding1
+    },
+    {
+        id: 2,
+        title: "O melhor serviço",
+        description: "Serviço de qualidade no melhor aplicativo de mobilidade urbana",
+        image: images.onboarding2
+    },
+    {
+        id: 3,
+        title: "Viaje com segurança",
+        description: "Segurança e confiabilidade",
+        image: images.onboarding3
+    },
+]
